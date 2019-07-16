8be22ab85a497380d954d3e070bfdc540e970c42
cmseeling <cmseeling@gmail.com>
2019-07-15 19:18:05 -0500

    added dialog to confirm delete actions

---
+++ src/components/ConfirmDeleteDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog v-model="isOpen" max-width="300">
+    <v-card>
+      <v-card-title class="headline">Are you sure you want to delete this {{itemTypeText}}?</v-card-title>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn color="success" @click="$emit('dialog:confirm')">Ok</v-btn>
+        <v-btn color="error" @click="$emit('dialog:cancel')">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'ConfirmDeleteDialog',
+  props: {
+    isOpen: Boolean,
+    itemTypeText: {
+      type: String,
+      default: 'item'
+    }
+  }
+});
+</script>
